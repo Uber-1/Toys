cc1f975d03a045b877979afbeda347eadcd2b0cc
Uber-1 <148308766+Uber-1@users.noreply.github.com>
2023-10-18 05:22:28 +0000

    Add files via upload

---
+++ autoconfig.js
@@ -0,0 +1,4 @@
+// autoconfig.js file needs to start with a comment
+pref("general.config.filename", "autoconfiglocal.js");
+pref("general.config.sandbox_enabled", false);
+pref("general.config.obscure_value", 0);
