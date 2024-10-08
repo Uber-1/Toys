97cc3f889b576081aa57a04c47ed9d9776637e19
Uber-1 <148308766+Uber-1@users.noreply.github.com>
2024-10-08 17:39:51 +0300

    Add files via upload

--- BAT_SA/FirefoxPortableCreator.bat
+++ BAT_SA/FirefoxPortableCreator.bat
@@ -2,8 +2,8 @@
 @title Firefox Portable Creator - ver.4.7.1 [08.10.2024]
 @cd /d "%~dp0"
 
-::  https://ftp.mozilla.org/pub/firefox/releases/latest-esr/README.txt
-::  product=firefox-esr-latest	- version
+::  https://ftp.mozilla.org/pub/firefox/releases/latest/README.txt
+::  product=firefox-latest	- version
 ::  os=win	- 32 bits
 ::  os=win64	- 64 bits
 ::  lang=en-US	- English
@@ -13,12 +13,12 @@
 @if exist "curl.exe" @GOTO CURLDL
 @if exist "%SystemRoot%\SYSTEM32\curl.exe" @GOTO CURLDL
 @echo Downloading with powershell . . .
-@powershell -Command "$wc = New-Object System.Net.WebClient; $wc.Headers.Add('referer','https://download.mozilla.org'); $wc.DownloadFile('https://download.mozilla.org/?product=firefox-esr-latest&os=win&lang=en-US', 'ffwin.exe.7z')"
+@powershell -Command "$wc = New-Object System.Net.WebClient; $wc.Headers.Add('referer','https://download.mozilla.org'); $wc.DownloadFile('https://download.mozilla.org/?product=firefox-latest&os=win&lang=en-US', 'ffwin.exe.7z')"
 @powershell -Command "(New-Object Net.WebClient).DownloadFile('https://www.7-zip.org/a/7zr.exe', '7zr.exe')"
 @GOTO MAKECONFIG
 :CURLDL
 @echo Downloading with CURL . . .
-@curl.exe -RL# "https://download.mozilla.org/?product=firefox-esr-latest&os=win&lang=en-US" -e"https://download.mozilla.org" -o "ffwin.exe.7z"
+@curl.exe -RL# "https://download.mozilla.org/?product=firefox-latest&os=win&lang=en-US" -e"https://download.mozilla.org" -o "ffwin.exe.7z"
 @curl.exe -RLO# "https://www.7-zip.org/a/7zr.exe"
 :MAKECONFIG
 
