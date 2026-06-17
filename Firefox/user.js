483d426688eef0a9d00048744b43567f7f5c3b1e
Uber-1 <148308766+Uber-1@users.noreply.github.com>
2026-06-17 18:09:11 +0300

    Add files via upload

--- Firefox/user.js
+++ Firefox/user.js
@@ -3,3 +3,7 @@
 user_pref("gfx.font_rendering.cleartype_params.force_gdi_classic_for_families", "Arial,Consolas,Courier New,Microsoft Sans Serif,Segoe UI,Tahoma,Trebuchet MS,Verdana");
 user_pref("gfx.font_rendering.cleartype_params.force_gdi_classic_max_size", 15);
 user_pref("sidebar.verticalTabs", false);
+
+user_pref("browser.startup.windowsLaunchOnLogin.defaultEnabled", false);
+user_pref("browser.startup.windowsLaunchOnLogin.enabled", false);
+user_pref("toolkit.winRegisterApplicationRestart", false);
